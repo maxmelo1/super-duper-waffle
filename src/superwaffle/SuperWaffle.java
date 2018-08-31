550d495f76c08ffa4fd5e968bf413c688b24f976
Homer Picaxu <>
2018-08-30 22:08:03 -0400

    bla bla

--- src/superwaffle/SuperWaffle.java
+++ src/superwaffle/SuperWaffle.java
@@ -7,7 +7,7 @@ package superwaffle;
 
 /**
  *
- * @author gin
+ * @author Homer PicaXU
  */
 public class SuperWaffle {
 
@@ -15,10 +15,10 @@ public class SuperWaffle {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        carro c = new carro();
         
         
-        //aaaaaaaaaaaaa
     }
     
 }
